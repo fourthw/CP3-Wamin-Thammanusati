0e1dddb7c6a75c957edd9fad27465ceedf4318cd
fourthw <fourthw44@gmail.com>
2021-01-15 12:02:22 +0700

    Add score

--- Excercise4_Wamin_Thammanusati.py
+++ Excercise4_Wamin_Thammanusati.py
@@ -1,1 +1,10 @@
-print("Hellow World")
+a = 100
+b = 100
+c = 100
+d = 100
+
+print("--- Your Score ---")
+print("Foundation English :", a)
+print("General Business :", b)
+print("Introduction to Computer System :", c)
+print("Computer Programming :", d)
